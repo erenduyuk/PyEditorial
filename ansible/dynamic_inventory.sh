14290b68489dfbc862267a1bdcdd40d04392b40f
Eren Duyuk <erenduyuk@hotmail.com>
2024-08-05 11:02:28 +0000

    Update 3 files
    - /ansible/deploy_prod.yml
    - /ansible/dynamic_inventory.sh
    - /.gitlab-ci.yml

--- ansible/dynamic_inventory.sh
+++ ansible/dynamic_inventory.sh
@@ -3,8 +3,6 @@
 cat <<EOF
 [production]
 ec2_prod ansible_host=$EC2_PRIVATE_IP_PROD ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_extra_args='-o StrictHostKeyChecking=no'
-
-[production1]
 ec2_prod1 ansible_host=$EC2_PRIVATE_IP_PROD1 ansible_user=ubuntu ansible_ssh_private_key_file=~/.ssh/id_rsa ansible_ssh_extra_args='-o StrictHostKeyChecking=no'
 
 [test]
