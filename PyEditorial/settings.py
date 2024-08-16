fb26330ad2a921524998b2bcdedc13b396187fd2
Eren Duyuk <erenduyuk@hotmail.com>
2024-08-16 07:24:07 +0000

    Update file settings.py

--- PyEditorial/settings.py
+++ PyEditorial/settings.py
@@ -53,7 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.HealthCheckMiddleware',
+    'Pyeditorial.middleware.HealthCheckMiddleware',
 ]
 
 ROOT_URLCONF = 'PyEditorial.urls'
